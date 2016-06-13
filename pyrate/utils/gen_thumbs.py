6464a552cce38372851bbfc849e9c96a24a828df
Garrick Paskos <Garrick.Paskos@ga.gov.au>
2016-06-13 12:13:18 +1000

    temp

--- pyrate/utils/gen_thumbs.py
+++ pyrate/utils/gen_thumbs.py
@@ -1,19 +1,22 @@
 from PIL import Image
 import os
 import numpy as np
-import matplotlib.pyplot as pp
+import matplotlib.pyplot as plt
 import sys
 from osgeo import gdal
+from os.path import join
 
 import pyrate.ifgconstants as ifc
 
-direc = r'C:\Users\gap\grk_viz\tif'
-direc = sys.argv[1]
+# will get below from argument
+#direc = r'C:\Users\gap\grk_viz\tif'
+#direc = sys.argv[1]
+direc = '/home/gap/pr_testing/tests/orbitalfit/syd_g/1/python'
 
 #print direc
 #while True: pass
 
-lev = 0
+#lev = 0
 for trav in os.walk(direc):
     #print trav
     break
@@ -22,10 +25,13 @@ for trav in os.walk(direc):
 root = trav[0]
 fns = trav[2]
 print root
-print fns
+#print fns
+
 #while True: pass
 
-ifg_1s, ts_cums, ts_vels, ts_incrs = [], [], [], []
+ifg_1s, ifg_2s, ts_cums, ts_vels, ts_incrs = [], [], [], [], []
+'''
+# this would be the better way of doing it but i'm too lazy to write tests for these metadata additions
 for fn in fns:
     ds = gdal.Open(os.path.join(root,fn))
     md = ds.GetMetadata()
@@ -37,26 +43,49 @@ for fn in fns:
         pass
     if md[ifc.PR_OUT_TYPE] == 'ts_incr':
         pass
+'''
+# can't actually identify ifg_1s from file name. so can't do
+for fn in fns:
+    if fn[-6:] == 'cr.tif':
+        ifg_2s.append(fn)
+    elif fn[:6] == 'tscuml':
+        ts_cums.append(fn)
+    elif fn[:5] == 'tsvel':
+        ts_vels.append(fn)
+    elif fn[:6] == 'tsincr':
+        ts_incrs.append(fn)
 
+print ifg_2s
+#print ts_cums
+#print ts_vels
+#print ts_incrs
+
+# create ifg_2 thumbnails...
+ifg_2s.sort()
+#print ifg_2s
+# write them out as 1/2 of 1920 = 960, then scale to what you want...
+# try 5 per row
+
+fig = plt.figure()
+fig_shape = (len(ifg_2s)/5, 5)     # rows, cols
+
+tif_dats = [join(root, fn)  np.array(tif.getdata()).reshape(tif.size[::-1]) for fn in fns]    # reverse shape. works. don't know why
+
+while True: pass
 
 print tifs
 
 # get actual data
-tif_dats = [np.array(tif.getdata()).reshape(tif.size[::-1]) for tif in tifs]    # reverse shape. works. don't know why
 
+
+'''
 #print tif_dats
-
 # matplotlibification
 fig = pp.figure()
 for it, tif_dat in enumerate(tif_dats):
     plt = fig.add_subplot(4, 5, it+1)
     pp.imshow(tif_dat)
 
-
 #print dir(plt)
-
 pp.show()
-
-
-
-#img = Image.open()
+'''
