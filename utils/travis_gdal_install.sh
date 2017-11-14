03871ae354b98ca09e93b7c1371cea21c3970e98
Sudipta Basak <basaks@gmail.com>
2017-11-14 18:27:08 +1100

    travis and gdal install script changes

--- utils/travis_gdal_install.sh
+++ utils/travis_gdal_install.sh
@@ -1,13 +1,10 @@
 #!/bin/sh
-#
-# originally contributed by @rbuffat to Toblerity/Fiona
 set -e
 
 GDALOPTS="  --with-ogr \
             --with-geos \
             --with-expat \
             --without-libtool \
-            --with-libz=internal \
             --with-libtiff=internal \
             --with-geotiff=internal \
             --without-gif \
@@ -17,13 +14,13 @@ GDALOPTS="  --with-ogr \
             --without-cfitsio \
             --without-pcraster \
             --without-netcdf \
-            --with-png=/usr \
+            --with-png=internal \
             --with-jpeg=internal \
             --without-gif \
             --without-ogdi \
             --without-fme \
             --without-hdf4 \
-            --with-hdf5 \
+            --without-hdf5 \
             --without-jasper \
             --without-ecw \
             --without-kakadu \
@@ -36,15 +33,14 @@ GDALOPTS="  --with-ogr \
             --without-xerces \
             --without-odbc \
             --with-curl \
-            --without-sqlite3 \
+            --with-sqlite3 \
             --without-dwgdirect \
             --without-idb \
             --without-sde \
             --without-perl \
             --without-php \
             --without-ruby \
-            --with-python \
-            --with-static-proj4=/usr/lib"
+            --without-python"
 
 # Create build dir if not exists
 if [ ! -d "$GDALBUILD" ]; then
@@ -57,17 +53,15 @@ fi
 
 ls -l $GDALINST
 
-if [ "$GDALVERSION" = "1.9.2" -a ! -d "$GDALINST/gdal-$GDALVERSION" ]; then
-  cd $GDALBUILD
-  wget http://download.osgeo.org/gdal/gdal-$GDALVERSION.tar.gz
-  tar -xzf gdal-$GDALVERSION.tar.gz
-  cd gdal-$GDALVERSION
+if [ "$GDALVERSION" = "trunk" ]; then
+  # always rebuild trunk
+  svn checkout https://svn.osgeo.org/gdal/trunk/gdal $GDALBUILD/trunk
+  cd $GDALBUILD/trunk
   ./configure --prefix=$GDALINST/gdal-$GDALVERSION $GDALOPTS
   make -s -j 2
   make install
-fi
-
-if [ "$GDALVERSION" != "1.9.2" -a ! -d "$GDALINST/gdal-$GDALVERSION" ]; then
+elif [ ! -d "$GDALINST/gdal-$GDALVERSION" ]; then
+  # only build if not already installed
   cd $GDALBUILD
   wget http://download.osgeo.org/gdal/$GDALVERSION/gdal-$GDALVERSION.tar.gz
   tar -xzf gdal-$GDALVERSION.tar.gz
